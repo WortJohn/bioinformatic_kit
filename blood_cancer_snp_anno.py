e543a2699505c4edff4b7d75c74e2fa773eed369
WortJohn <1004794592@qq.com>
2017-07-11 17:47:33 +0800

    revise blood_cancer_snp_anno.py

--- blood_cancer_snp_anno.py
+++ blood_cancer_snp_anno.py
@@ -2,12 +2,13 @@
 #-*-coding: utf-8 -*-
 from __future__ import  print_function
 from cal_gn_for_bed import PrintException
-import sys, re
+import sys, os, re
 import argparse, configparser
 import pandas as pd
 
 __author__ = 'Wort John'
 __date__ = '2017-06-13'
+__email__ = 'shuirong.zhang@majorbio.com'
 
 def output_result(anno_result, opf):
     anno_result.to_csv(opf, sep='\t', index=False)
@@ -38,13 +39,55 @@ def pre_handle_tab(tab):
 
 def read_config(probe):
     config = configparser.ConfigParser(interpolation=configparser.BasicInterpolation())
-    config.read('~/.config.ini')
+    config.read('/mnt/ilustre/users/shuirong.zhang/run/blood_cancer/.config.ini')
     if re.search(r'dna', probe, re.I):
         return config['DEFAULT']['dna']
     else:
         return config['DEFAULT']['rna']
 
-def main(ipf, probe, opf, tab0):
+def stat_infos(id, probe):
+    import ctDNA_report
+    import logging
+    import mymodule
+    import add_data2XA
+    import cal_each_gene_cov
+    import subprocess
+    try:
+        probe = add_data2XA.read_probe_file(mymodule.read_config_XA(), probe)
+        # 计算每个基因的覆盖度
+        cal_each_gene_cov.main(str(id)+'.snp.vcf.tab', probe, id)
+        # 统计表格
+        sample = ctDNA_report.Table(id=id, bed=probe, sample_type='XA')
+        sample.region_tlen(id + ".isize.xls")
+        sample.bedcov_probe(id + ".valid.bam")
+        sample.bedcov_probe_ratio(id + ".bedcov_probe.tmp.xls")
+        sample.bedcov_gene(id + ".bedcov_probe.xls")
+        os.remove(id + ".bedcov_probe.tmp.xls")
+        # 统计图
+        sample = ctDNA_report.Plot(id, sample_type='XA')
+        sample.plot_tlen(id + ".tlen0_500.xls")
+        sample.plot_region_tlen(id + ".tlen_region.xls")
+        sample.plot_depth(id + ".snp.vcf.tab")
+        # 对Q20 Q30 GC等计算raw和clean的
+        sample = ctDNA_report.Table(id + "_raw")
+        sample.qc(id + "_R1.fastq.gz", id + "_R2.fastq.gz")
+        sample = ctDNA_report.Plot(id + "_raw")
+        sample.extract_base_plot(id + "_R1_fastqc.zip")
+        
+        sample = ctDNA_report.Table(id + "_clean")
+        sample.qc(id + "_R1.clean.fq.gz", id + "_R2.clean.fq.gz")
+        sample = ctDNA_report.Plot(id + "_clean")
+        sample.extract_base_plot(id + "_R1.clean_fastqc.zip")
+        
+        cmd = 'tail -1 {id}_clean.Q20_Q30_GC.xls >> {id}_raw.Q20_Q30_GC.xls; mv {id}_raw.Q20_Q30_GC.xls {id}.Q20_Q30_GC.xls;rm {id}_clean.Q20_Q30_GC.xls; sed -i "s/#//g" {id}.Q20_Q30_GC.xls'.format(id=id)
+        subprocess.call(cmd, shell=True)
+
+        # 生成md文件
+        ctDNA_report.md_file(id, 'XA')
+    except Exception as e:
+        logging.exception(e)
+
+def main(ipf, probe, opf, tab0, stat):
     try:
         anno_file = read_config(probe)
         data = pre_handle_tab(ipf)
@@ -63,10 +106,14 @@ def main(ipf, probe, opf, tab0):
         sys.exit(0)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='The program is used for anntating snp of blood cancer sample. If you don\'t need the depth info before deduplicate, don\'t add the "-d" option.', epilog='Example: blood_cancer_snp_anno.py -i 1.snp.vcf.tab -t dna -d 1.snp0.vcf.tab')
+    parser = argparse.ArgumentParser(description='The program is used for anntating snp of blood cancer sample. If you don\'t need the depth info before deduplicate, don\'t add the "-d" option.', epilog='Example: blood_cancer_snp_anno.py -i 1.snp.vcf.tab -t dna -d 1.snp0.vcf.tab --sample_name 1 -s')
     parser.add_argument('-i', dest='tab_file', help='input file likes "1.snp.vcf.tab"', required=True)
     parser.add_argument('-t', dest='type', help='probe type which is used for hybrid capture', choices=['DNA', 'dna', 'RNA', 'rna'], required=True)
     parser.add_argument('-d', dest='tab0_file', help='input file likes "1.snp0.vcf.tab" (the tab file before deduplicate)')
     parser.add_argument('-o', dest='output_file', help='The result file after annotating. If you don\'t give "-o" option, the name of output file likes "1.snp.vcf.ann.tab" which is according to the file name of "-i" option')
+    parser.add_argument('--sample_name', help='Analysis id of each sample', required=True)
+    parser.add_argument('-s', dest='statistics', help='Statistics other informations through ctDNA_report module', action='store_const', const='yes', default='no')
     args = parser.parse_args()
-    main(args.tab_file, args.type, args.output_file, args.tab0_file)
+    main(args.tab_file, args.type, args.output_file, args.tab0_file, args.statistics)
+    if args.statistics == 'yes':
+        stat_infos(args.sample_name, args.type)
